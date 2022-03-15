feb72453f5a5b20cddc1ce3b1d3d811e869c3a6f
Zachary Sirna <zsirna@yahoo.com>
2022-03-15 13:00:43 -0400

    commit tree 2fefc57264b6ec5b83051ae214f3d8cb6297e7b9

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  ?
 */
 
 /* 
@@ -16,7 +22,6 @@ function hasTargetSum(array, target) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
